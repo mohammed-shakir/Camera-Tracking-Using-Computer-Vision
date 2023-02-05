d926f6efef262693da8980ebb651c406df5b5a41
mohameddianwi <dinawi@me.com>
2023-02-05 10:49:22 +0100

    commented out old code

--- camera_control/controller.py
+++ camera_control/controller.py
@@ -20,6 +20,8 @@ class Controller(Observer):
         # Change frame rate for better performance
         self.src = "http://130.240.105.144/cgi-bin/mjpeg?resolution=1920x1080&amp;framerate=5&amp;quality=1"
 
+        self.cam = HDIntegratedCamera("http://130.240.105.144/cgi-bin/aw_ptz?cmd=%23")
+
         self.camera_bedroom_pos = numpy.array([619, 3935, 2600])
         self.camera_bedroom_zero = numpy.array([-765, 4112, 2878])
         self.camera_bedroom_floor = numpy.array([531, 3377, 331])
@@ -28,18 +30,17 @@ class Controller(Observer):
         self.camera_kitchen_zero = numpy.array([3413, -2722, 2284])
         self.camera_kitchen_floor = numpy.array([2694, -2722, 193])
 
-        self.cam = HDIntegratedCamera("http://130.240.105.144/cgi-bin/aw_ptz?cmd=%23")
         # self.cam_trans = wf.Transform(self.camera_kitchen_pos, self.camera_kitchen_zero, self.camera_kitchen_floor)
 
         self.rotate(210, 140)
 
-        self.trackers = []
-        self.trackersDict = {}
+        # self.trackers = []
+        # self.trackersDict = {}
 
         self.rot_amount = 6
 
-        self.followTarget = ""
-        self.is_follow = False
+        # self.followTarget = ""
+        # self.is_follow = False
 
     #  need to be deleted
 
@@ -90,24 +91,24 @@ class Controller(Observer):
 
     # Handling of manual input with arrow keys
     def up(self):
-        self.is_follow = False
+        # self.is_follow = False
         self.cam.rotate(self.cam.get_current_yaw(), self.cam.get_current_pitch() + self.rot_amount)
 
     def down(self):
-        self.is_follow = False
+        # self.is_follow = False
         self.cam.rotate(self.cam.get_current_yaw(), self.cam.get_current_pitch() - self.rot_amount)
 
     def left(self):
-        self.is_follow = False
+        # self.is_follow = False
         self.cam.rotate(self.cam.get_current_yaw() - self.rot_amount, self.cam.get_current_pitch())
 
     def right(self):
-        self.is_follow = False
+        # self.is_follow = False
         self.cam.rotate(self.cam.get_current_yaw() + self.rot_amount, self.cam.get_current_pitch())
 
     # Handling of zoom in and zoom out on interface
     def zoomIn(self):
-        self.cam.zoom(50)
+        self.cam.zoom(100)
 
     def zoomOut(self):
         self.cam.zoom(0)
